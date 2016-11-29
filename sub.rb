044f76635732cf8a50a8ef31163d928dc77a08dd
Jonathan Felchlin <jonathan@energsavvy.com>
2016-11-29 00:07:30 -0800

    Version bump

--- sub.rb
+++ sub.rb
@@ -1,11 +1,12 @@
 class Sub < Formula
   desc ""
   homepage ""
-  url "https://github.com/GreenGremlin/sub/archive/v0.1.0.tar.gz"
+  url "https://github.com/GreenGremlin/sub/archive/v0.1.1.tar.gz"
   # sha256 "85cc828a96735bdafcf29eb6291ca91bac846579bcef7308536e0c875d6c81d7"
 
   def install
-    bin.install "bin/sub"
-    bash_completion.install "completions/sub.bash" => "sub"
+    bin.install "sub.sh" => "sub"
+    bin.install "mksub.sh" => "mksub"
+    bash_completion.install "completions/sub.bash" => "sub-completions"
   end
 end
