7b79ee6bf6e75971aedd699f8f7e498bafc88165
Jonathan Felchlin <jonathan@energsavvy.com>
2016-11-29 03:51:05 -0800

    Version bump

--- sub.rb
+++ sub.rb
@@ -1,7 +1,7 @@
 class Sub < Formula
   desc ""
   homepage ""
-  url "https://github.com/GreenGremlin/sub/archive/v0.1.8.tar.gz"
+  url "https://github.com/GreenGremlin/sub/archive/v0.1.9.tar.gz"
   head "https://github.com/GreenGremlin/sub.git"
   sha256 "f2dc06ec57a5d7f0d9fee20f67eba781fd48d43d80a248c6f7668c4734095745"
 
