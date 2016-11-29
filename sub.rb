70ff425f7c3d5f22791e04523fd68fc38bf3d9a6
Jonathan Felchlin <jonathan@energsavvy.com>
2016-11-29 03:37:06 -0800

    Adding checksum

--- sub.rb
+++ sub.rb
@@ -3,7 +3,7 @@ class Sub < Formula
   homepage ""
   url "https://github.com/GreenGremlin/sub/archive/v0.1.8.tar.gz"
   head "https://github.com/GreenGremlin/sub.git"
-  # sha256 "ac387e05074e93095bb61ada9adf0f3d05d748e5893c93e4d88e2766a8f262dc"
+  sha256 "f2dc06ec57a5d7f0d9fee20f67eba781fd48d43d80a248c6f7668c4734095745"
 
   def install
     bin.install "bin/sub" => "sub"
