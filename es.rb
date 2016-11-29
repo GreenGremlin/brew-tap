6471f6366e25683fb857820bb88ee0816b11ea5e
Jonathan Felchlin <jonathan@energsavvy.com>
2016-11-29 00:47:22 -0800

    adding es recipe

---
+++ es.rb
@@ -0,0 +1,14 @@
+class Es < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/GreenGremlin/es/archive/v0.1.0.tar.gz"
+  head "https://github.com/GreenGremlin/es.git"
+  # sha256 "ac387e05074e93095bb61ada9adf0f3d05d748e5893c93e4d88e2766a8f262dc"
+  depends_on "greengremlin/cask/sub"
+
+  def install
+    libexec.install Dir["*"]
+    es_command = `mksub es #{libexec}`
+    print es_command
+  end
+end
--- sub.rb
+++ sub.rb
@@ -1,8 +1,8 @@
 class Sub < Formula
   desc ""
   homepage ""
-  url "https://github.com/GreenGremlin/sub/archive/v0.1.1.tar.gz"
-  sha256 "ac387e05074e93095bb61ada9adf0f3d05d748e5893c93e4d88e2766a8f262dc"
+  url "https://github.com/GreenGremlin/sub/archive/v0.1.2.tar.gz"
+  # sha256 "ac387e05074e93095bb61ada9adf0f3d05d748e5893c93e4d88e2766a8f262dc"
 
   def install
     bin.install "sub.sh" => "sub"
